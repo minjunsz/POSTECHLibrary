bea126554dfa257ceb38b6d3680c98c5c184dd93
HyeonSuShin <hoya134679@gmail.com>
2020-03-21 07:22:36 +0000

    add bodyParser

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
-// import badyParser from "body-parser";
+import badyParser from "body-parser";
 import { Controller } from "./controller/main.controller";
+import bodyParser from "body-parser";
 
 class App {
   public app: express.Application;
 
   constructor() {
     this.app = express();
-
+    this.SetConfig();
     new Controller(this.app);
+  }
+
+  private SetConfig() {
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
   }
 }
 
