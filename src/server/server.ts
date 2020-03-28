ec5e89d66ac93a9a7284d59751de9815cf3ecba0
조채은 <jochaeeun@jochaeeun-ui-MacBookPro.local>
2020-03-28 15:43:19 +0900

    [DB] Add login query reuturns dummy data

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,21 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
 import app from "./app";
 import { PORT } from "./constants";
+import { buildSchema } from "type-graphql";
+import { SeatResolver } from "./resolvers/order.resolver";
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+(async () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ SeatResolver ]
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+   });
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+})();
