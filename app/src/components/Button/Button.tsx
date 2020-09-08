971bb169d872441d82828e3692d74cb1140fd5a6
root <root@gram15-minjun.localdomain>
2020-09-09 04:08:55 +0900

    add custom button

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import './button.css';
+import styles from './Button.module.scss';
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
+  primary?: boolean,
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
@@ -34,11 +19,14 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode: string = primary ? styles.storybookButtonPrimary : styles.storybookButtonSecondary;
+  const sizeMode: string = size === 'small' ? styles.storybookButtonSmall : (
+    size === 'medium' ? styles.storybookButtonMedium : styles.storybookButtonLarge)
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        `${styles.storybookButton}`, sizeMode, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
