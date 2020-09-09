777c113e1a5fbefc1b382f000572e141caea5dbd
root <root@gram15-minjun.localdomain>
2020-09-10 06:07:02 +0900

    add a11y addon & sort story by name

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,3 +1,12 @@
+import { addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+
+addDecorator(withA11y);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 }
